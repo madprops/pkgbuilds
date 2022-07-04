4283bb9c4f2a09d7faa3f61a6690d86f642c2448
Auric Vente <mprops@protonmail.com>
2022-07-04 01:36:55 -0600

    Mods

---
+++ backup.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+rm -R backup
+mkdir backup
+mkdir backup/git
+mkdir backup/bin
+
+cd git 
+
+for d in */ ; do
+  mkdir ../backup/git/"$d"
+  cp "$d"/PKGBUILD ../backup/git/$d/PKGBUILD
+  cp "$d"/.gitignore ../backup/git/$d/.gitignore
+  cp "$d"/push.sh ../backup/git/$d/push.sh
+done
+
+cd ../bin 
+
+for d in */ ; do
+  mkdir ../backup/bin/"$d"
+  cp "$d"/PKGBUILD ../backup/bin/$d/PKGBUILD
+  cp "$d"/.gitignore ../backup/bin/$d/.gitignore
+  cp "$d"/push.sh ../backup/bin/$d/push.sh
+done
---
+++ push.sh
@@ -0,0 +1,5 @@
+git add backup/*
+git add backup.sh
+git add push.sh
+git commit -m "Mods"
+git push origin main
